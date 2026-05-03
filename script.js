11fc41121a441b6fa41661e61af1c759c590d3c6
laimamanna-svg <laimamanna@gmail.com>
2026-05-03 19:46:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ function updateTime() {
 }
 
 setInterval(updateTime, 1000);
+// SIGNUP
+function signup() {
+    let email = document.getElementById("newEmail").value;
+    let password = document.getElementById("newPassword").value;
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    alert("Account created!");
+    window.location.href = "login.html";
+}
+
+// LOGIN
+function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let storedEmail = localStorage.getItem("email");
+    let storedPassword = localStorage.getItem("password");
+
+    if (email === storedEmail && password === storedPassword) {
+        alert("Login successful!");
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid login!");
+    }
+}
+
+// LOGOUT
+function logout() {
+    alert("Logged out!");
+    window.location.href = "index.html";
+}
